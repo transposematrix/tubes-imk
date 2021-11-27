67ca1f14a65a64b25756d37c55fe17a26708e89b
Amanda <christineamanda26@gmail.com>
2021-11-27 15:01:47 +0700

    Mengubah tampilan index sekretaris

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\letter_in;
+use App\Models\letter_out;
+use App\Models\report;
+use App\User;
+use Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class HomeController extends Controller
 {
@@ -35,7 +44,11 @@ class HomeController extends Controller
     }
     public function halamansekretaris()
     {
-        return view('admin.sekretaris.sekretaris-index');
+        $letter_in = letter_in::count();
+        $letter_out = letter_out::count();
+        $report = report::count();
+        $member = User::where('category', '!=', 'alumnee')->count();
+        return view('admin.sekretaris.sekretaris-index', compact('letter_in', 'letter_out', 'report', 'member'));
     }
     public function halamanbendahara()
     {
